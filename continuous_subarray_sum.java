037600e7dc66a4b930f2c00bb27734763182ecec
Camille Anderson <camz.anderson@gmail.com>
2020-09-27 17:11:29 -0500

    Update and rename continuous_subarray_sum to continuous_subarray_sum.java
    updating description

--- continuous_subarray_sum.java
+++ continuous_subarray_sum.java
@@ -2,6 +2,10 @@
 * "Given a list of non-negative numbers and a target integer k, write a function 
 * to check if the array has a continuous subarray of size at least 2 that sums up 
 * to a multiple of k, that is, sums up to n*k where n is also an integer." - Leetcode problem 523
+*
+* The theorum enabling us to solve this problem in one pass is that if we have an interval i,j
+* where j-i>1 and the continuous_sum[i] % k == continuous_sum[j] % k, then the sum of i,j is 
+* divisible by k.
 ***/
 
 class Solution {
@@ -18,7 +22,7 @@ class Solution {
             Integer prev = remainderToIndex.get(r);
             
             if(prev != null){
-                if(i - prev > 1) return true;
+                if(i-prev > 1) return true;
             }
             else{
                 remainderToIndex.put(r, i);
